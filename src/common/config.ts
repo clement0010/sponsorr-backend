ca29319fc79ccf211bd60fe9cdd932c5467f25df
Clement Tee <clement@usefriz.com>
2021-07-19 06:24:10 +0800

    feat: add enum config value for email service

--- src/common/config.ts
+++ src/common/config.ts
@@ -5,18 +5,32 @@ import { LogType } from './types';
 export const log = (type: LogType, message: string, data?: Record<string, unknown>): void => {
   switch (type) {
     case 'info':
-      logger.info(message, data);
+      logger.info(message, data || {});
 
       break;
     case 'error':
-      logger.error(message, data);
+      logger.error(message, data || {});
 
       break;
     case 'warn':
-      logger.warn(message, data);
+      logger.warn(message, data || {});
 
       break;
   }
 };
 
 export const REGION = 'asia-southeast2';
+
+export enum Status {
+  Accepted = 'accepted',
+  Rejected = 'rejected',
+  Pending = 'pending',
+}
+
+export enum EventStatus {
+  Published = 'published',
+  Draft = 'draft',
+  Matched = 'matched',
+}
+
+export const EMAIL_SOURCE = 'Sponsorr! <clement.jdp15@gmail.com>';
