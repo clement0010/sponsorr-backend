7336b16c1fb4019cfc0cfef8229055b2ecc85072
Clement Tee <clement@usefriz.com>
2021-07-21 05:31:42 +0800

    feat: add webhook url generator and seed db function

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,5 +1,14 @@
-import { EMAIL_SOURCE } from './config';
-import { Attachment, MatchedEmailNotificationObject, MatchedEmailParams } from './types';
+import { createHmac } from 'crypto';
+
+import { BASE_URL, EMAIL_SOURCE } from './config';
+import {
+  Attachment,
+  ConfirmationEmailObject,
+  MatchedEmailNotificationObject,
+  MatchedEmailParams,
+  SponsorEvents,
+} from './types';
+import { baseEvent } from '../test/db.fixtures';
 
 export const buildEmailNotificationObject = (
   userId: string,
@@ -22,4 +31,45 @@ export const buildEmailNotificationObject = (
   }
 
   return emailObject;
+};
+
+export const buildConfirmationEmailObject = (
+  userId: string,
+  username: string,
+  webhookUrl: string,
+): ConfirmationEmailObject => {
+  const emailObject: ConfirmationEmailObject = {
+    from: EMAIL_SOURCE,
+    toUids: [userId],
+    template: {
+      name: 'confirmation',
+      data: { username, webhookUrl },
+    },
+  };
+
+  return emailObject;
+};
+
+export const generateEvents = (userId: string, length: number): SponsorEvents => {
+  const result: SponsorEvents = [];
+
+  for (let i = 0; i < length; i++) {
+    result.push({
+      userId,
+      ...baseEvent(),
+    });
+  }
+  return result;
+};
+
+export const generateWebhookHash = (email: string, uid: string, secret: string): string => {
+  const hmac = createHmac('sha256', secret);
+  hmac.update(email).update(uid);
+
+  return hmac.digest('hex');
+};
+
+export const generateWebhookUrl = (email: string, uid: string, secret: string): string => {
+  const hash = generateWebhookHash(email, uid, secret);
+  return `${BASE_URL}confirmation?hash=${hash}&emailAddress=${email}&id=${uid}`;
 };
