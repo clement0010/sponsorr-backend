0a918e195f94a1c991e4c7133e5e807f95d8cb8b
Clement Tee <clement@usefriz.com>
2021-07-21 05:29:05 +0800

    feat: populate db function

---
+++ src/populateDb.ts
@@ -0,0 +1,40 @@
+import * as express from 'express';
+import * as cors from 'cors';
+
+import { functions, log, populateEventDatabase, populateUserDatabase } from './common';
+import {
+  eventOrganiser1Auth,
+  eventOrganiser2Auth,
+  sponsor1Auth,
+  sponsor2Auth,
+} from './test/db.fixtures';
+import { apiValidation } from './middleware';
+
+const app = express();
+
+app.use(cors({ origin: true }));
+app.use(apiValidation);
+
+app.post('/', async (req: express.Request, res: express.Response) => {
+  // Grab the text parameter.
+  log('info', 'Incoming populate db request');
+  try {
+    Promise.all([
+      populateUserDatabase(eventOrganiser1Auth),
+      populateUserDatabase(eventOrganiser2Auth),
+      populateUserDatabase(sponsor1Auth),
+      populateUserDatabase(sponsor2Auth),
+      populateEventDatabase(eventOrganiser1Auth),
+      populateEventDatabase(eventOrganiser2Auth),
+    ]);
+    log('info', 'Completed db population job');
+
+    res.status(201).json({
+      message: 'Saved items to db',
+    });
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+});
+
+export const populateDb = functions.https.onRequest(app);
